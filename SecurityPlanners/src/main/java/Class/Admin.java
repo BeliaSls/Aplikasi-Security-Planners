48330bcc429424544b2dac0a55f7efe00862aba2
Dwi Apriliani Putri <dwiaprlniptri@gmail.com>
2024-06-18 12:41:12 +0700

    admin

--- SecurityPlanners/src/main/java/Class/Admin.java
+++ SecurityPlanners/src/main/java/Class/Admin.java
@@ -9,5 +9,19 @@ package Class;
  * @author User
  */
 public class Admin {
-    
+    private String username;
+    private String password;
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean login(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
 }
